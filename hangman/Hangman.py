e56ddac3c291f77d6b9a335a8be7ea54188859dd
unknown <gabe.millikan@geoffandcindy.com>
2019-10-23 15:05:37 -0700

    child friendly hangman

--- hangman/Hangman.py
+++ hangman/Hangman.py
@@ -1,137 +1,85 @@
 """
+-> dummed down to be easier to understand
 
--create an array
--print array
--append to array
--access index of array
--get length of array
--loop thru array
+Overview:
 
--create string "is an array"
--access index of string
--get length of string
--loop thru letters in string
--"append" to string (+=)
-
-
--on each iteration of string, print if letter.isalpha()
--print letter.lower()/.upper()
-
--compare each letter to 'h' or 'e'
--make a class that prints input
--only accepts 1 letter
--only accepts alphabetical
-
--class stores all the letters that you pressed
--removes duplicates
-
-g
-
+-function to get a letter
+-check for repeated letters
+-check if we completed the word
+-get display word
 """
 
-hi = 'hi'
-
-
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-class Word():
-    guessed_letters = []
-
-    def __init__(self, word):
-        self.word = word
-        self.finished = False
-        self.remaining_guesses = 10
+# Gets a single letter from an input
+def guess_letter():
+    letter_guess = ""
+    
+    while letter_guess == "":
+        entered = input("Guess a letter: ")
         
-    def already_guessed(self, check):
-        for letter in self.guessed_letters:
-            if letter.lower() == check.lower():
-                return True
-        return False
-        
-    def letters_left(self):
-        left = []
-        
-        for letter in alphabet:
-            if not self.already_guessed(letter):
-                left.append(letter)
-                
-        return left
-        
-    def get_print_word(self):
-        out = ""
-        for letter in self.word:
-            if self.already_guessed(letter):
-                out += letter
+        if len(entered) == 1:
+            if entered.isalpha():
+                letter_guess = entered
             else:
-                if not letter.isalpha():
-                    out+=letter
-                else:
-                    out += "*"
-            
-        return out
-        
-    def guess_correct(self, check):
-        for letter in self.word:
-            if letter.lower() == check.lower():
-                return True
+                print("Only the English alphabet will be accepted")
+        else:
+            print("Only a single letter is allowed")
                 
-        return False
-        
-        
-    def play(self):
-        print("\nThe word is: ", end = '')
-        print(self.get_print_word())
-        
-        print("Remaining guesses: ", self.remaining_guesses)
-        
-        print("Letter Bank: ", end = '')
-        for l in self.letters_left():
-            print(l, end=' ')
-        print()
-    
-        guess = input("guess a letter: ")
-        if not guess.isalpha():
-            print("\nplease only enter letters!\n")
-            return
-        if not len(guess):
-            print("\nyou gotta enter something\n")
-            return
-        if len(guess) > 1:
-            print("\nPlease only enter one letter\n")
-            return
-        
-        if self.already_guessed(guess):
-            print("\nYou already guessed that!\n")
-            return            
-            
-        self.guessed_letters.append(guess)
-        self.guessed_letters.sort()
-        if not self.guess_correct(guess):
-            self.remaining_guesses -= 1
-        
-        if (self.get_print_word().lower() == self.word.lower()):
-            print()
-            print("You win!")
-            print("The word was : '" + self.word + "'")
+    return letter_guess.lower()
+
+# test this function by adding dummy letters
+guessed_letters = []
+def is_letter_guessed(letter):
+    for already_guessed in guessed_letters:
+        if already_guessed.lower() == letter.lower():
             return True
-            
-        if self.remaining_guesses <= 0:
-            print()
-            print("You lose!")
-            print("The word was : '" + self.word + "'")
-            return True
-            
-            
-        return False
-            
-            
-round = Word("Example 'word' with spaces and punctuation.")
-finished = False
-while not finished:
-    finished = round.play()
+    return False
     
+word_to_guess = "Hello"
     
+def is_word_completed():
+    for letter in word_to_guess:
+        if letter.isalpha():
+            if not is_letter_guessed(letter):
+                return False
+    return True
     
+def get_display_word():
+    display_word = ''
     
+    for letter in word_to_guess:
+        if is_letter_guessed(letter) or not letter.isalpha():
+            display_word = display_word + letter
+        else:
+            display_word = display_word + "_"
+        
+    return display_word
+
+guesses_left = 10
     
+word_completed = False
+while not word_completed:
+    print("Your word: ", get_display_word())
+    print("Remaining guesses: ", guesses_left)
+    guess = guess_letter()
+    already_guessed = is_letter_guessed(guess)
+    if not already_guessed:
+        guessed_letters.append(guess)
+    else:
+        print("You already guessed that letter!")
+        continue
+        
+    
+    #check if we finished the word
+    word_completed = is_word_completed()
+    if word_completed:
+        print("You win! The word was: ", word_to_guess)
+        break;
+    
+    guesses_left = guesses_left - 1
+    if (guesses_left == 0):
+        print("You rand out of guesses!")
+        break;
+    print("")
+
+    
+
