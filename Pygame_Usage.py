ebaf7aea397005527deccd7723b52fc168734ff5
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2019-12-03 23:41:01 -0800

    motion

--- Pygame_Usage.py
+++ Pygame_Usage.py
@@ -4,6 +4,14 @@ pygame.init()
 background = pygame.display.set_mode((800,500))
 clock = pygame.time.Clock()
 
+def getKeys():
+	pressed_keys = pygame.key.get_pressed()
+	#119: W
+	#97:  A
+	#115: S
+	#100: D
+	#32:  space
+	return [pressed_keys[119],pressed_keys[97],pressed_keys[115],pressed_keys[100],pressed_keys[32]]
 
 class color:
 	black = (0, 0, 0)
@@ -13,6 +21,10 @@ class color:
 	blue = (0,0,255)
 	def new(r,g,b):
 		return (r,g,b) #possibly the most useless function
+
+partpos = [0,0]
+partvel = [0,0]
+partacc = [0,0.1]
 
 class image:
 	img1 = pygame.image.load('img.jpg')
@@ -33,6 +45,31 @@ while True:
 			pygame.quit()
 	background.fill(color.white)
 
-	draw(image.img1, mouse_pos())
+	partpos = [partpos[0] + partvel[0], partpos[1] + partvel[1]]
+	partvel = [partvel[0] + partacc[0], partvel[1] + partacc[1]]
+	partacc[0] = (getKeys()[3]-getKeys()[1])/2
+	partvel[0]/=1.1
+	if partvel[0] > 5:
+		partvel[0] = 5
+	if partvel[0] < -5:
+		partvel[0] = -5
+	#hit the ground
+	if partpos[1] >= 500-50:
+		partpos[1] = 500-50
+		partvel[1] = 0
+		#jump
+		if getKeys()[-1]:
+			partvel[1] -= 5
+	#hit a wall
+	if not(0 <= partpos[0] <= 800-50):
+		partvel[0] = 0
+		partacc[0] = 0
+		if partpos[0] > 0:
+			partpos[0] = 800-50
+		else:
+			partpos[0] = 0
+
+
+	draw(image.img1, partpos)
 	pygame.display.update()
 	clock.tick(60)
